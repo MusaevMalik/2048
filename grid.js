3cf394eac986ab6c41c8957cf585e0ceb0b12417
Musaev Malik <musaevmalikd@gmail.com>
2023-04-20 03:54:23 +0600

    second

---
+++ grid.js
@@ -0,0 +1,11 @@
+const GRID_SIZE = 4;
+const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
+
+export class Grid {
+  constructor(gridElement) {
+    this.cells = [];
+    for (let i = 0; i < CELLS_COUNT; i++) {
+      this.cells.push(new this.cells(gridElement, x, y));
+    }
+  }
+}
