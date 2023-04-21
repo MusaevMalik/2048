0d4c626b8779056a96e9c1ddd92ed23020fa398d
Musaev Malik <musaevmalikd@gmail.com>
2023-04-21 12:57:48 +0600

    step 3

--- grid.js
+++ grid.js
@@ -1,3 +1,5 @@
+import { Cell } from "./cell.js";
+
 const GRID_SIZE = 4;
 const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
 
@@ -5,7 +7,15 @@ export class Grid {
   constructor(gridElement) {
     this.cells = [];
     for (let i = 0; i < CELLS_COUNT; i++) {
-      this.cells.push(new this.cells(gridElement, x, y));
+      this.cells.push(
+        new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
+      );
     }
+  }
+
+  getRandomEmptyCell() {
+    const emptyCells = this.cells.filter((cell) => cell.isEmpty());
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    return emptyCells[randomIndex];
   }
 }
